c137db9215ec5f51c01357dc1e01b82c4da5b213
Julia Desmazes <julia.desmazes@gmail.com>
2025-11-02 21:52:28 -0600

    fix dma rd bug, wasn't enabling dma channel, victory

--- firmware/data_rd_utils.c
+++ firmware/data_rd_utils.c
@@ -14,7 +14,7 @@ uint init_rd_dma_channel(PIO pio, uint sm){
 	channel_config_set_dreq(&c, pio_get_dreq(pio, sm, false)); // trigger dreq when there is an valid entry in the RX FIFO
 	dma_channel_set_read_addr(dma_chan, &pio->rxf[sm], false);
 	channel_config_set_high_priority(&c, true); // prioritize receiving hash over sending data/config
-	channel_config_set_enable(&c, false); 
+	channel_config_set_enable(&c, true); 
 	dma_channel_set_config(dma_chan, &c, false);
 	return dma_chan; 
 }
@@ -22,11 +22,15 @@ uint init_rd_dma_channel(PIO pio, uint sm){
 void setup_rd_dma_hash_stream(uint dma_chan, uint nn, uint8_t* buffer, size_t bl){
 	size_t tc = (nn + PIO_FIFO_W-1)/ PIO_FIFO_W;	
 	hard_assert(tc <= bl*PIO_FIFO_W);
+	hard_assert(tc);
+	hard_assert(buffer);
 	// clear hash, helps with debug
 	memset(buffer, 0, bl);
 	hard_assert(!dma_channel_is_busy(dma_chan));
 	dma_channel_set_write_addr(dma_chan, buffer, false);
 	dma_channel_set_transfer_count(dma_chan, tc, true);
+
+	
 }
 
 void read_hash(uint8_t* hash, uint8_t nn, uint8_t *buffer, size_t bl, uint dma_chan){
